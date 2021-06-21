b77017b7813cfbb2dff3353b36abe1aae99d46e5
Mamadou Dian SOW <mdiansow@gmail.com>
2021-06-21 17:56:46 +0200

    Update IDs and bucketes

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "mdsow-buildongcp-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="mdsow-buildongcp"
